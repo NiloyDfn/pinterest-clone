1062b01bce833dfa82b1d09b97505032c1f2899d
NiloyDfn <147066379+NiloyDfn@users.noreply.github.com>
2024-06-28 12:44:19 +0600

    Dynamic project

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const plm = require('passport-local-mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/pinterest-niloy-clone');
 
-
 const userSchema = mongoose.Schema({
     username : String,
     name : String,
